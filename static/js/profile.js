08d92e11e58758bf5996099fa1a65f4bde28754c
rzlucas <guezlucas@gmail.com>
2023-09-29 02:45:39 -0300

    ya wueygit add .

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,25 +1,22 @@
-async function fetchUserProfile(event) {
-    event.preventDefault(); // Esto prevendrá la recarga de la página
-    
+async function fetchUserProfile() {
     const userId = document.getElementById('userId').value;
     if (!userId) {
-        alert('Por favor ingrese un ID válido');
+        alert('Please enter a user ID');
         return;
     }
 
     try {
         const response = await fetch(`http://127.0.0.1:5000/users/${userId}`);
         if (!response.ok) {
-            alert('El usuario no existe');
+            alert('User not found');
             return;
         }
 
         const data = await response.json();
-        document.getElementById('username').textContent = data.username;
-        document.getElementById('email').textContent = data.email;
-        document.getElementById('profilePicture').src = data.profilePicture;
-        document.getElementById('user-details').style.display = 'block';
-        document.getElementById('profilePictureContainer').style.display = 'block';
+        document.getElementById('username').textContent = data.Username;
+        document.getElementById('email').textContent = data.Email;
+        document.getElementById('profilePicture').src = data.ProfilePicture;
+        document.getElementById('user-profile').style.display = 'block';
     } catch (error) {
         console.error('Error:', error);
     }
@@ -28,29 +25,30 @@ async function fetchUserProfile(event) {
 async function editField(field) {
     let value;
     if (field === 'ProfilePicture') {
-        value = prompt('Ingresa la nueva URL de la imagen');
-        if (!value) return;
+        value = prompt('Enter the new image URL');
     } else {
-        value = prompt(`Ingresa nuevo ${field}`);
+        value = prompt(`Enter new ${field}`);
     }
 
-    const userId = document.getElementById('userId').value;
-    try {
-        const response = await fetch(`http://127.0.0.1:5000/users/${userId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ [field]: value })
-        });
+    if (value) {
+        const userId = document.getElementById('userId').value;
+        try {
+            const response = await fetch(`http://127.0.0.1:5000/users/${userId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ [field]: value })
+            });
 
-        if (response.ok) {
-            alert(`${field} se actualizó correctamente!`);
-            fetchUserProfile(new Event('click')); // Simula un evento de clic para refrescar la información
-        } else {
-            alert('Falló al actualizar el perfil');
+            if (response.ok) {
+                alert(`${field} updated successfully!`);
+                fetchUserProfile();
+            } else {
+                alert('Failed to update profile');
+            }
+        } catch (error) {
+            console.error('Error:', error);
         }
-    } catch (error) {
-        console.error('Error:', error);
     }
 }
