2f17c58ccd691c7ed5301de77a3dc02d8ae4d23c
rzlucas <guezlucas@gmail.com>
2023-09-29 00:15:20 -0300

    profile

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,5 +1,26 @@
-function changePassword() {
-    // Aquí puedes añadir la lógica para cambiar la contraseña
-    // Por ejemplo, mostrando un modal o redirigiendo a otra página
-    alert('Aquí podrías abrir un modal para cambiar la contraseña o redirigir al usuario a una página de cambio de contraseña.');
+async function fetchUserProfile() {
+    const userId = document.getElementById('userId').value;
+    if (!userId) {
+        alert('Please enter a user ID');
+        return;
+    }
+
+    try {
+        const response = await fetch(`http://127.0.0.1:5000/users/${userId}`);
+        if (!response.ok) {
+            alert('User not found');
+            return;
+        }
+
+        const data = await response.json();
+        document.getElementById('username').textContent = data.Username;
+        document.getElementById('email').textContent = data.Email;
+        document.getElementById('profilePicture').src = data.ProfilePicture;
+        document.getElementById('user-profile').style.display = 'block';
+
+        // Esta línea hace que el div de la imagen del perfil sea visible
+        document.getElementById('profilePictureContainer').style.display = 'block';
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
