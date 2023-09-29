63a3efcc9f52b9feb16533d0a908d9ffaf402002
rzlucas <guezlucas@gmail.com>
2023-09-29 00:35:53 -0300

    js actualizado

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,14 +1,14 @@
 async function fetchUserProfile() {
     const userId = document.getElementById('userId').value;
     if (!userId) {
-        alert('Please enter a user ID');
+        alert('Por favor ingrese un ID válido');
         return;
     }
 
     try {
         const response = await fetch(`http://127.0.0.1:5000/users/${userId}`);
         if (!response.ok) {
-            alert('User not found');
+            alert('El usuario noo existe');
             return;
         }
 
@@ -22,5 +22,37 @@ async function fetchUserProfile() {
         document.getElementById('profilePictureContainer').style.display = 'block';
     } catch (error) {
         console.error('Error:', error);
+    }
+}
+
+
+async function editField(field) {
+    let value;
+    if (field === 'ProfilePicture') {
+        value = prompt('Enter the new image URL');
+    } else {
+        value = prompt(`Enter new ${field}`);
+    }
+
+    if (value) {
+        const userId = document.getElementById('userId').value;
+        try {
+            const response = await fetch(`http://127.0.0.1:5000/users/${userId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ [field]: value })
+            });
+
+            if (response.ok) {
+                alert(`${field} updated successfully!`);
+                fetchUserProfile();
+            } else {
+                alert('Failed to update profile');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 }
